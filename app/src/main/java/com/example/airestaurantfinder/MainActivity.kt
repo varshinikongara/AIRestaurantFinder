259df9d356c1f15091ea554771da3aef6a1ea182
varshinikongara <varshinikongara@gmail.com>
2026-03-10 23:54:18 +0530

    developed splash screen

---
+++ app/src/main/java/com/example/airestaurantfinder/MainActivity.kt
@@ -0,0 +1,180 @@
+package com.example.airestaurantfinder
+
+import android.app.Activity
+import android.content.Intent
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import com.example.airestaurantfinder.ui.theme.AIRestaurantFinderTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.jvm.java
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AIRestaurantFinderTheme {
+                MainScreen()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MainScreen() {
+    var splashStatus by remember { mutableStateOf(true) }
+
+    val context = LocalContext.current as Activity
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+        splashStatus = false
+    }
+
+    if (splashStatus) {
+        SplashScreen()
+    } else {
+
+//        val currentStatus = StressLevelTesterData.readLS(context)
+//
+//        if (currentStatus) {
+//            openHome(context)
+//        } else {
+//            openLogin(context)
+//
+//        }
+    }
+}
+
+fun openLogin(context: Activity) {
+//    context.startActivity(Intent(context, LoginActivity::class.java))
+//    context.finish()
+}
+
+fun openHome(context: Activity) {
+//    context.startActivity(Intent(context, BaseActivity::class.java))
+//    context.finish()
+}
+
+@Composable
+fun SplashScreen() {
+
+//    val context = LocalContext.current as Activity
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.p1)),
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Spacer(modifier = Modifier.height(100.dp))
+
+            Text(
+                text = "AI Restaurant Finder",
+                color = colorResource(id = R.color.white),
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Column(
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterHorizontally)
+                    .background(
+                        color = colorResource(id = R.color.white),
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .border(
+                        width = 1.dp,
+                        color = colorResource(id = R.color.white),
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                    .padding(16.dp),
+
+                )
+            {
+                Image(
+                    modifier = Modifier.size(200.dp, 200.dp),
+                    painter = painterResource(id = R.drawable.restaurant_finder),
+                    contentDescription = "Grocery Store Manager",
+                )
+
+
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+
+
+            Text(
+                text = "By",
+                color = colorResource(id = R.color.white),
+                style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+            Text(
+                text = "Dummy Dummy",
+                color = colorResource(id = R.color.white),
+                style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+
+
+        }
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SplashScreenPreview() {
+    SplashScreen()
+}
