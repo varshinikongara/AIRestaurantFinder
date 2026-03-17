e34d36c2de56135902092404ed02b10f1c07942b
varshinikongara <varshinikongara@gmail.com>
2026-03-18 00:44:15 +0530

    implemented authentication screens

---
+++ app/src/main/java/com/example/airestaurantfinder/RegistrationScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.airestaurantfinder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegistrationScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
