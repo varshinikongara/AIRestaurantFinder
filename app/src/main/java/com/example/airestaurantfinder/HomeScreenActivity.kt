e8ec41bc915dc0b27da1dc793addf05de8b6caee
varshinikongara <varshinikongara@gmail.com>
2026-03-26 16:21:59 +0530

    developed home screen

---
+++ app/src/main/java/com/example/airestaurantfinder/HomeScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.example.airestaurantfinder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
